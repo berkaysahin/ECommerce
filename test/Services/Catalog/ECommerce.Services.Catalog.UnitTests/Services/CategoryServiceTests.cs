bd6809c4957b39430799feb89a6db4237188437e
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-13 15:44:55 +0300

    change folder structure and create Catalog unit test project

---
+++ test/Services/Catalog/ECommerce.Services.Catalog.UnitTests/Services/CategoryServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ECommerce.Services.Catalog.UnitTests.Services;
+
+public class CategoryServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
