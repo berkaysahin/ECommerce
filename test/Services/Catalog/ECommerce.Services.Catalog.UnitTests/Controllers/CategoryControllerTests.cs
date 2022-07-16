4201f9a3628dd180598f3e3dfa09ee6e1aaaaa67
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-16 15:33:30 +0300

    create CategoryControllerTests

---
+++ test/Services/Catalog/ECommerce.Services.Catalog.UnitTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,140 @@
+using System.Linq.Expressions;
+using AutoMapper;
+using ECommerce.Services.Catalog.Controllers;
+using ECommerce.Services.Catalog.DTOs;
+using ECommerce.Services.Catalog.Interfaces;
+using ECommerce.Services.Catalog.Mapping;
+using ECommerce.Services.Catalog.Models;
+using ECommerce.Services.Catalog.Services;
+using ECommerce.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ECommerce.Services.Catalog.UnitTests.Controllers;
+
+public class CategoryControllerTests
+{
+    private readonly CategoryController _sut;
+    private readonly Mock<IMongoDbClient<Category>> _mongoDbClientMock = new Mock<IMongoDbClient<Category>>();
+
+    public CategoryControllerTests()
+    {
+        var mappingConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new MappingProfile());
+        });
+
+        IMapper mapper = mappingConfig.CreateMapper();
+
+        ICategoryService categoryService = new CategoryService(mapper, _mongoDbClientMock.Object);
+        _sut = new CategoryController(categoryService);
+    }
+    
+    [Test]
+    public async Task GetAll_ShouldReturnCategoryCount_WhenCategoriesExist()
+    {
+        // Arrange
+        Category category = new Category()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Lorem"
+        };
+
+        _mongoDbClientMock
+            .Setup(f => f.FindAsync(It.IsAny<Expression<Func<Category, bool>>>()))
+            .ReturnsAsync(new List<Category>() { category });
+
+        // Act
+        var categoryResult = (ObjectResult)(await _sut.GetAll());
+
+        // Assert
+        Assert.GreaterOrEqual(((Response<List<CategoryDTO>>)(categoryResult.Value)).Data.Count, 1);
+    }
+
+    #region GetByIdAsync
+
+    [Test]
+    public async Task GetById_ShouldReturnCategory_WhenCategoryExist()
+    {
+        // Arrange
+        Category category = new Category()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Lorem"
+        };
+
+        _mongoDbClientMock
+            .Setup(f => f.FindByIdAsync(It.IsAny<Expression<Func<Category, bool>>>()))
+            .ReturnsAsync(category);
+
+        // Act
+        var categoryResult = (ObjectResult)(await _sut.GetById(category.Id));
+
+        // Assert
+        Assert.That(((Response<CategoryDTO>)(categoryResult.Value)).Data.Id, Is.EqualTo(category.Id));
+    }
+
+    [Test]
+    public async Task GetById_ShouldReturnFail_WhenCategoryDoesntExist()
+    {
+        // Arrange
+        Category category = new Category()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Lorem"
+        };
+
+        _mongoDbClientMock
+            .Setup(f => f.FindByIdAsync(It.IsAny<Expression<Func<Category, bool>>>()))
+            .ReturnsAsync(null as Category);
+
+        // Act
+        var categoryResult = (ObjectResult)(await _sut.GetById(category.Id));
+
+        // Assert
+        Assert.That(((Response<CategoryDTO>)(categoryResult.Value)).StatusCode, Is.EqualTo(404));
+    }
+
+    #endregion
+
+    #region CreateAsync
+
+    [Test]
+    public async Task Create_ShouldReturnCategory_WhenCategoryCreated()
+    {
+        // Arrange
+        CategoryDTO category = new CategoryDTO()
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Lorem"
+        };
+
+        _mongoDbClientMock
+            .Setup(f => f.InsertOneAsync(It.IsAny<Category>()))
+            .Returns(Task.FromResult(typeof(void)));
+
+        // Act
+        var categoryResult = (ObjectResult)(await _sut.Create(category));
+
+        // Assert
+        Assert.NotNull(((Response<CategoryDTO>)(categoryResult.Value)).Data);
+    }
+
+    [Test]
+    public async Task Create_ShouldReturnFail_WhenCategoryNotCreated()
+    {
+        // Arrange
+
+        _mongoDbClientMock
+            .Setup(f => f.InsertOneAsync(It.IsAny<Category>()))
+            .Returns(Task.FromResult(typeof(void)));
+
+        // Act
+        var categoryResult = (ObjectResult)(await _sut.Create(null as CategoryDTO));
+
+        // Assert
+        Assert.Null(((Response<CategoryDTO>)(categoryResult.Value)).Data);
+    }
+
+    #endregion
+}
