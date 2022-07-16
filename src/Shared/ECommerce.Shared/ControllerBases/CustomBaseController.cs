7d26473399bb9e7abc96bf980b10d3aecf61a2ea
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-16 14:47:40 +0300

    create CustomBaseController

---
+++ src/Shared/ECommerce.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,16 @@
+using ECommerce.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
