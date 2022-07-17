c7c5e6c257e7e43b7ddd4fb7d1c24a194977b436
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-17 14:10:49 +0300

    create SignUpDTO in IdentityServer

---
+++ src/IdentityServer/ECommerce.IdentityServer/DTOs/SignUpDTO.cs
@@ -0,0 +1,10 @@
+namespace ECommerce.IdentityServer.DTOs
+{
+    public class SignUpDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
