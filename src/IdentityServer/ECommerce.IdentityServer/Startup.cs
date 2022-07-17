bb4d85542b6d911c8325646172042832e86569f7
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-17 18:34:59 +0300

    add AddResourceOwnerValidator to IdentityServer

--- src/IdentityServer/ECommerce.IdentityServer/Startup.cs
+++ src/IdentityServer/ECommerce.IdentityServer/Startup.cs
@@ -5,6 +5,7 @@
 using IdentityServer4;
 using ECommerce.IdentityServer.Data;
 using ECommerce.IdentityServer.Models;
+using ECommerce.IdentityServer.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -54,6 +55,8 @@ namespace ECommerce.IdentityServer
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerPasswordValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
