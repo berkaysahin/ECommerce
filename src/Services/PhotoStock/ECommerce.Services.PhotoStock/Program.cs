1820b3dfdedcc6b94e523559beae036db0b0bcd8
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-18 15:00:15 +0300

    add authentication to PhotoStock

--- src/Services/PhotoStock/ECommerce.Services.PhotoStock/Program.cs
+++ src/Services/PhotoStock/ECommerce.Services.PhotoStock/Program.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "photo_stock_catalog";
+    options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new AuthorizeFilter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +32,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
