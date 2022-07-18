b31cd70794ee4fc5b29d1d9e0e346e23b3b66c4f
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-18 22:12:23 +0300

    configure host and port settings for Redis in Basket

--- src/Services/Basket/ECommerce.Services.Basket/Program.cs
+++ src/Services/Basket/ECommerce.Services.Basket/Program.cs
@@ -1,3 +1,6 @@
+using System.Configuration;
+using ECommerce.Services.Basket.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
 
 var app = builder.Build();
 
