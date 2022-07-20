26bd739c604e530527e09c1570f11f70037aef8e
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-20 12:51:34 +0300

    create IDiscountService

---
+++ src/Services/Discount/ECommerce.Services.Discount/Interfaces/IDiscountService.cs
@@ -0,0 +1,13 @@
+using ECommerce.Shared.DTOs;
+
+namespace ECommerce.Services.Discount.Interfaces;
+
+public interface IDiscountService
+{
+    Task<Response<List<Models.Discount>>> GetAll();
+    Task<Response<Models.Discount>> GetById(int id);
+    Task<Response<NoContent>> Save(Models.Discount discount);
+    Task<Response<NoContent>> Update(Models.Discount discount);
+    Task<Response<NoContent>> Delete(int id);
+    Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+}
