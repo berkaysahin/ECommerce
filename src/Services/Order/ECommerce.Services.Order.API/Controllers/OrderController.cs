fb07607cf7e477856a33aec95bf787e383bce25f
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-25 23:24:37 +0300

    create OrderController

---
+++ src/Services/Order/ECommerce.Services.Order.API/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Services.Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
