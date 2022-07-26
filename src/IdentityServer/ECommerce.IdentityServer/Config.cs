65214154bff62324bb4692f4ccc0ef21206d61bf
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-26 21:51:14 +0300

    configure authentication for FakePayment

--- src/IdentityServer/ECommerce.IdentityServer/Config.cs
+++ src/IdentityServer/ECommerce.IdentityServer/Config.cs
@@ -18,6 +18,7 @@ namespace ECommerce.IdentityServer
             new ApiResource("resource_basket"){ Scopes = {"basket_fullpermission"} },
             new ApiResource("resource_discount"){ Scopes = {"discount_fullpermission"} },
             new ApiResource("resource_order"){ Scopes = {"order_fullpermission"} },
+            new ApiResource("resource_payment"){ Scopes = {"payment_fullpermission"} },
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -44,6 +45,7 @@ namespace ECommerce.IdentityServer
                 new ApiScope("basket_fullpermission", "full access for Basket API"),
                 new ApiScope("discount_fullpermission", "full access for Discount API"),
                 new ApiScope("order_fullpermission", "full access for Order API"),
+                new ApiScope("payment_fullpermission", "full access for Payment API"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
             };
 
@@ -70,6 +72,7 @@ namespace ECommerce.IdentityServer
                         "basket_fullpermission",
                         "discount_fullpermission",
                         "order_fullpermission",
+                        "payment_fullpermission",
                         IdentityServerConstants.StandardScopes.Email, 
                         IdentityServerConstants.StandardScopes.OpenId, 
                         IdentityServerConstants.StandardScopes.Profile,
