8fc83fb96ae393cc2fc5eab1920f988b3ef96c34
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-26 22:27:40 +0300

    add configuration files for gateway settings to Gateway project

--- src/Gateways/ECommerce.Gateway/Program.cs
+++ src/Gateways/ECommerce.Gateway/Program.cs
@@ -3,6 +3,12 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    var env = hostingContext.HostingEnvironment;
+    config.AddJsonFile($"configuration.{env.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+});
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
