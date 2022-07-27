05430a786ab3bd3133efaa28a3d2852b8bffa54c
Berkay Şahin <iletisim@berkaysahin.info>
2022-07-27 22:58:52 +0300

    create CreateOrderMessageCommand class in Shared

---
+++ src/Shared/ECommerce.Shared/Messages/CreateOrderMessageCommand.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace ECommerce.Shared.Messages
+{
+    public class CreateOrderMessageCommand
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public Address Address { get; set; }
+    }
+    
+    public class OrderItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureURL { get; set; }
+        public Decimal Price { get; set; }
+    }
+    
+    public class Address
+    {
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Line { get; set; }
+    }
+}
